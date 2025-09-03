a5abd9d0bb80603787db67cd69a1d702e6f54f7c
Fero Hriadel <ferdinand.hriadel@gmail.com>
2025-09-03 21:54:00 +0200

    serve client from /

--- api/Program.cs
+++ api/Program.cs
@@ -1,6 +1,7 @@
 using Api.Extensions;
 using Api.Middleware;
 using API.Extensions;
+using Microsoft.Extensions.FileProviders;
 
 
 
@@ -23,4 +24,29 @@ app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 app.UseStaticFiles(); // Serve static files from wwwroot
+// Serve Angular app from wwwroot/browser
+app.UseDefaultFiles(new DefaultFilesOptions {
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "browser")
+    ),
+    RequestPath = ""
+});
+app.UseStaticFiles(new StaticFileOptions
+{
+  FileProvider = new PhysicalFileProvider(
+        Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "browser")
+    ),
+  RequestPath = ""
+});
+// Redirect all non-API, non-static requests to Angular index.html
+app.Use(async (context, next) =>
+{
+    if (!context.Request.Path.StartsWithSegments("/api") &&
+        !context.Request.Path.StartsWithSegments("/images") &&
+        !Path.HasExtension(context.Request.Path.Value))
+    {
+        context.Request.Path = "/index.html";
+    }
+    await next();
+});
 app.Run();
